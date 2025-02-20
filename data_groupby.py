ac48ef6abfc2267269ec0a04cf6687b9123b1e34
Bo Lin <learldu91@gmail.com>
2025-02-20 10:19:20 +0800

    完成資料拆分的程式

---
+++ data_groupby.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#讀取已處理的Excel文件
+df = pd.read_excel("cleaned_example.xlsx")    #確保cleaned_example.xlsx在當前工作目錄中
+
+#按年齡分組
+grouped = df.groupby('年齡')
+
+#分別印出14歲和15歲的同學資料
+print("14歲的同學：")
+print(grouped.get_group(14))
+
+print("\n15歲的同學：")
+print(grouped.get_group('15'))
