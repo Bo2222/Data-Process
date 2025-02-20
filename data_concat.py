c35ff9606d9c1f61c388ec869ee92c8c9c17a433
Bo Lin <learldu91@gmail.com>
2025-02-20 10:12:05 +0800

    完成資料合併的程式

---
+++ data_concat.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+#讀取已處理過的Excel文件
+df = pd.read_excel("cleaned_example.xlsx")    #確保cleaned_example.xlsx在當前工作目錄中
+
+#新增班級成員
+new_student = pd.DataFrame({
+    '名字': ['Roni'],
+    '年齡': [15],
+    '學籍總成績': [100]
+})
+
+#將新學生資料添加到原有的DataFrame
+df = pd.concat([df, new_student], ignore_index=True)
+
+#打印合併後的結果
+print("合併後的結果：")
+print(df)
